4844171222ffa6aa1f165af877458bb20f614ffb
Ana Berdnik <ana.berdnik@gmail.com>
2017-07-24 20:52:21 +0200

    Zadnji popravki na programu

--- moj_kalkulator.py
+++ moj_kalkulator.py
@@ -67,53 +67,53 @@ class kalkulator:
 
     def __init__(self):
         
-        okno=Tk()
+        okno = Tk()
         okno.title('Kalkulator')
-        okno.configure(background='white')
-        okno.resizable(0,0)
+        okno.configure(background = 'white')
+        okno.resizable(0, 0)
         #onemogoča povečanje okna
 
         
-        self.error=False
+        self.error = False
         #ta oznaka self.error je ustvarjena za primere napak
-        #uporabnik bo za preklic Error-ja uporabil le AC
+        #uporabnik bo za preklic Error-ja uporabil le Clear
         #na drugih mestih bomo učinek preklicali
         
-        self.string=StringVar()
+        self.string = StringVar()
 
-        e=Entry(okno,textvariable=self.string, width=30,
-                bd=10,bg='white', justify=RIGHT, font='Helvetica 15 bold')
-        e.grid(row=0, columnspan=6)
+        e = Entry(okno, textvariable = self.string, width = 30,
+                bd = 10, bg = 'white', justify = RIGHT, font = 'Helvetica 15 bold')
+        e.grid(row = 0, columnspan = 6)
         e.bind('<KeyPress>', self.keyPress)
         e.focus()
         #focus usmeri začetno dogajanje
 
 
-        #bg=background color
-        #bd=border
-        Button(okno,text="=",bg='light blue',width=14,height=2, command=lambda:self.enačaj()).grid(row=3,column=5,rowspan=2, sticky='NWNESWSE')
-        Button(okno,text='Clear',width=5,height=2, command=lambda:self.počisti_vse()).grid(row=1, column=5, sticky='NWNESWSE')
-        Button(okno,text='<-',width=5,height=2, command=lambda:self.pobriši_zadnji_element()).grid(row=2, column=5, sticky='NWNESWSE')
-        Button(okno,text="+",width=5,height=2, command=lambda:self.dodaj_vrednost('+')).grid(row=3, column=3, sticky='NWNESWSE')
-        Button(okno,text="*",width=5,height=2, command=lambda:self.dodaj_vrednost('*')).grid(row=4, column=3, sticky='NWNESWSE')
-        Button(okno,text="-",width=5,height=2, command=lambda:self.dodaj_vrednost('-')).grid(row=3, column=4, sticky='NWNESWSE')
-        Button(okno,text="/",width=5,height=2, command=lambda:self.dodaj_vrednost('/')).grid(row=4, column=4, sticky='NWNESWSE') 
-        Button(okno,text="%",width=5,height=2, command=lambda:self.dodaj_vrednost('%')).grid(row=4, column=2, sticky='NWNESWSE')
-        Button(okno,text="7",bg='light blue',width=5,height=2, command=lambda:self.dodaj_vrednost(7)).grid(row=1, column=0, sticky='NWNESWSE')
-        Button(okno,text="8",bg='light blue',width=5,height=2, command=lambda:self.dodaj_vrednost(8)).grid(row=1, column=1, sticky='NWNESWSE')
-        Button(okno,text="9",bg='light blue',width=5,height=2, command=lambda:self.dodaj_vrednost(9)).grid(row=1, column=2, sticky='NWNESWSE')
-        Button(okno,text="4",bg='light blue',width=5,height=2, command=lambda:self.dodaj_vrednost(4)).grid(row=2, column=0, sticky='NWNESWSE')
-        Button(okno,text="5",bg='light blue',width=5,height=2, command=lambda:self.dodaj_vrednost(5)).grid(row=2, column=1, sticky='NWNESWSE')
-        Button(okno,text="6",bg='light blue',width=5,height=2, command=lambda:self.dodaj_vrednost(6)).grid(row=2, column=2, sticky='NWNESWSE')
-        Button(okno,text="1",bg='light blue',width=5,height=2, command=lambda:self.dodaj_vrednost(1)).grid(row=3, column=0, sticky='NWNESWSE')
-        Button(okno,text="2",bg='light blue',width=5,height=2, command=lambda:self.dodaj_vrednost(2)).grid(row=3, column=1, sticky='NWNESWSE')
-        Button(okno,text="3",bg='light blue',width=5,height=2, command=lambda:self.dodaj_vrednost(3)).grid(row=3, column=2, sticky='NWNESWSE')
-        Button(okno,text=".",width=5,height=2,command=lambda:self.dodaj_vrednost('.')).grid(row=4, column=0, sticky='NWNESWSE')
-        Button(okno,text="0",bg='light blue',width=5,height=2, command=lambda:self.dodaj_vrednost(0)).grid(row=4, column=1, sticky='NWNESWSE')
-        Button(okno,text="(",width=5,height=2, command=lambda:self.dodaj_vrednost('(')).grid(row=1, column=3, sticky='NWNESWSE')
-        Button(okno,text=")",width=5,height=2, command=lambda:self.dodaj_vrednost(')')).grid(row=1, column=4, sticky='NWNESWSE')
-        Button(okno,text="√",width=5,height=2, command=lambda:self.dodaj_vrednost('√')).grid(row=2, column=3, sticky='NWNESWSE')
-        Button(okno,text="^",width=5,height=2, command=lambda:self.dodaj_vrednost('^')).grid(row=2, column=4, sticky='NWNESWSE')
+        #bg = background color
+        #bd = border
+        Button(okno, text = "=", bg = 'light blue', width = 14, height = 2, command = lambda: self.enačaj()).grid(row = 3,column = 5,rowspan = 2, sticky = 'NWNESWSE')
+        Button(okno, text = 'Clear', width = 5, height = 2, command = lambda: self.počisti_vse()).grid(row = 1, column = 5, sticky = 'NWNESWSE')
+        Button(okno, text = '<-', width = 5, height = 2, command = lambda: self.pobriši_zadnji_element()).grid(row = 2, column = 5, sticky = 'NWNESWSE')
+        Button(okno, text = "+", width = 5, height = 2, command = lambda: self.dodaj_vrednost('+')).grid(row = 3, column = 3, sticky = 'NWNESWSE')
+        Button(okno, text = "*", width = 5, height = 2, command = lambda: self.dodaj_vrednost('*')).grid(row = 4, column = 3, sticky = 'NWNESWSE')
+        Button(okno, text = "-", width = 5, height = 2, command = lambda: self.dodaj_vrednost('-')).grid(row = 3, column = 4, sticky = 'NWNESWSE')
+        Button(okno, text = "/", width = 5, height = 2, command = lambda: self.dodaj_vrednost('/')).grid(row = 4, column = 4, sticky = 'NWNESWSE') 
+        Button(okno, text = "%", width = 5, height = 2, command = lambda: self.dodaj_vrednost('%')).grid(row = 4, column = 2, sticky = 'NWNESWSE')
+        Button(okno, text = "7", bg = 'light blue', width = 5, height = 2, command = lambda: self.dodaj_vrednost(7)).grid(row = 1, column = 0, sticky = 'NWNESWSE')
+        Button(okno, text = "8", bg = 'light blue', width = 5, height = 2, command = lambda: self.dodaj_vrednost(8)).grid(row = 1, column = 1, sticky = 'NWNESWSE')
+        Button(okno, text = "9", bg = 'light blue', width = 5, height = 2, command = lambda: self.dodaj_vrednost(9)).grid(row = 1, column = 2, sticky = 'NWNESWSE')
+        Button(okno, text = "4", bg = 'light blue', width = 5, height = 2, command = lambda: self.dodaj_vrednost(4)).grid(row = 2, column = 0, sticky = 'NWNESWSE')
+        Button(okno, text = "5", bg = 'light blue', width = 5, height = 2, command = lambda: self.dodaj_vrednost(5)).grid(row = 2, column = 1, sticky = 'NWNESWSE')
+        Button(okno, text = "6", bg = 'light blue', width = 5, height = 2, command = lambda: self.dodaj_vrednost(6)).grid(row = 2, column = 2, sticky = 'NWNESWSE')
+        Button(okno, text = "1", bg = 'light blue', width = 5, height = 2, command = lambda: self.dodaj_vrednost(1)).grid(row = 3, column = 0, sticky = 'NWNESWSE')
+        Button(okno, text = "2", bg = 'light blue', width = 5, height = 2, command = lambda: self.dodaj_vrednost(2)).grid(row = 3, column = 1, sticky = 'NWNESWSE')
+        Button(okno, text = "3", bg = 'light blue', width = 5, height = 2, command = lambda: self.dodaj_vrednost(3)).grid(row = 3, column = 2, sticky = 'NWNESWSE')
+        Button(okno, text = ".", width = 5, height = 2, command = lambda: self.dodaj_vrednost('.')).grid(row = 4, column = 0, sticky = 'NWNESWSE')
+        Button(okno, text = "0", bg = 'light blue', width = 5, height = 2, command = lambda: self.dodaj_vrednost(0)).grid(row = 4, column = 1, sticky = 'NWNESWSE')
+        Button(okno, text = "(", width = 5, height=2, command = lambda: self.dodaj_vrednost('(')).grid(row = 1, column = 3, sticky = 'NWNESWSE')
+        Button(okno, text = ")", width = 5, height=2, command = lambda: self.dodaj_vrednost(')')).grid(row = 1, column = 4, sticky = 'NWNESWSE')
+        Button(okno, text = "√", width = 5, height=2, command = lambda: self.dodaj_vrednost('√')).grid(row = 2, column = 3, sticky = 'NWNESWSE')
+        Button(okno, text = "^", width = 5, height=2, command = lambda: self.dodaj_vrednost('^')).grid(row = 2, column = 4, sticky = 'NWNESWSE')
 
         #z opcijo sticky lahko lepo razporedimo gumbe; 'NWNESWSE' so smeri neba
 
@@ -127,11 +127,11 @@ class kalkulator:
    
     def počisti_vse(self):
         self.string.set('')
-        self.error=False
+        self.error = False
 
     def pobriši_zadnji_element(self):
         if(not self.error):
-            self.string.set(self.string.get()[0:-1])
+            self.string.set(self.string.get()[0 : -1])
 
     
 
@@ -147,24 +147,24 @@ class kalkulator:
             vpisano = pretvori_niz(vpisano)
 
         if '^' in vpisano:
-            vpisano= vpisano.replace('^', '**')
+            vpisano = vpisano.replace('^', '**')
 
         if '%' in vpisano:
             vpisano = vpisano.replace('%','*0.01')
             
         try:
-            rezultat=eval(vpisano)
+            rezultat = eval(vpisano)
         except:
-            self.error=True
-            rezultat='Error'
+            self.error = True
+            rezultat = 'Error'
                 
         self.string.set(rezultat)
 
     def dodaj_vrednost(self, vrednost):
         if(not self.error):
-            self.string.set(self.string.get()+(str(vrednost)))
+            self.string.set(self.string.get() + (str(vrednost)))
 
-    def keyPress(self,event):
+    def keyPress(self, event):
         return 'break'
         #ob pritisku na tipkovnico ne izpiše ničesar
         
